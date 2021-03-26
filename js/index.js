afc1096fdea6e827424793486c8639f676c58e41
Uchechukwu Ottah <coderboy.exe@gmail.com>
2021-03-26 19:30:26 +0100

    adjusted responsiveness. unable to implement nav dropdown

---
+++ js/index.js
@@ -0,0 +1,8 @@
+/*function dropDown() {
+    var x = document.getElementById("nav");
+    if (x.classname === "navbar") {
+        x.className += " responsive";
+    } else {
+        x.className = "navbar";
+    }
+}*/
